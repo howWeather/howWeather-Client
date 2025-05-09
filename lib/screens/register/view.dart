1bb242f77d901df8fbdc01df6405a3873a8acb51
kimjihyun <kimjihyun@MacBook-Pro.local>
2025-05-09 13:57:53 +0900

    feat: 착장 선택하기 dialog (#19)

--- lib/screens/register/view.dart
+++ lib/screens/register/view.dart
@@ -10,6 +10,8 @@ final isAllValidProvider = Provider<bool>((ref) {
   return true;
 });
 final selectedTemperatureProvider = StateProvider<String?>((ref) => null);
+final selectedClothProvider = StateProvider<String?>((ref) => null);
+final selectedClothInfoProvider = StateProvider<String?>((ref) => null);
 
 class Register extends ConsumerWidget {
   Register({super.key});
@@ -111,7 +113,7 @@ class Register extends ConsumerWidget {
     return GestureDetector(
       onTap: isAllValid
           ? () {
-              context.push('/calendar');
+              context.go('/calendar');
             }
           : null,
       child: Container(
@@ -189,17 +191,27 @@ class Register extends ConsumerWidget {
         SizedBox(
           height: 8,
         ),
-        Container(
-          padding: EdgeInsets.symmetric(vertical: 8),
-          width: MediaQuery.of(context).size.width * 0.4,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(12),
-            border: Border.all(color: HowWeatherColor.black, width: 1),
-          ),
+        InkWell(
+          onTap: () {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return ClothModal(context, ref);
+              },
+            );
+          },
           child: Container(
-              alignment: Alignment.centerRight,
-              padding: EdgeInsets.only(right: 8),
-              child: SvgPicture.asset("assets/icons/search.svg")),
+            padding: EdgeInsets.symmetric(vertical: 8),
+            width: MediaQuery.of(context).size.width * 0.4,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              border: Border.all(color: HowWeatherColor.black, width: 1),
+            ),
+            child: Container(
+                alignment: Alignment.centerRight,
+                padding: EdgeInsets.only(right: 8),
+                child: SvgPicture.asset("assets/icons/search.svg")),
+          ),
         ),
         SizedBox(
           height: 8,
@@ -209,6 +221,186 @@ class Register extends ConsumerWidget {
           child: Image.asset("assets/images/windbreak.png"),
         ),
       ],
+    );
+  }
+
+  Widget ClothModal(BuildContext context, WidgetRef ref) {
+    return AlertDialog(
+      backgroundColor: HowWeatherColor.white,
+      title: Center(child: Semibold_20px(text: "상의 선택하기")),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ClothCard("민소매", ref),
+          SizedBox(
+            height: 12,
+          ),
+          ClothCard("반소매", ref),
+          SizedBox(
+            height: 12,
+          ),
+          ClothCard("긴소매", ref),
+          SizedBox(
+            height: 12,
+          ),
+          ClothCard("니트", ref),
+          SizedBox(
+            height: 12,
+          ),
+          ClothCard("조끼", ref),
+          SizedBox(
+            height: 12,
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: GestureDetector(
+                  onTap: () => context.pop(),
+                  child: Container(
+                    padding: EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      color: HowWeatherColor.neutral[200],
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Center(child: Medium_14px(text: "취소")),
+                  ),
+                ),
+              ),
+              SizedBox(width: 8),
+              Expanded(
+                child: GestureDetector(
+                  onTap: () {
+                    context.push('/calendar/register');
+                  },
+                  child: Container(
+                    padding: EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      color: HowWeatherColor.primary[900],
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Center(
+                      child: Medium_14px(
+                        text: "등록",
+                        color: HowWeatherColor.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget ClothCard(text, WidgetRef ref) {
+    final selected = ref.watch(selectedClothProvider);
+
+    final isSelected = selected == text;
+
+    return InkWell(
+      onTap: () {
+        ref.read(selectedClothProvider.notifier).state =
+            isSelected ? null : text;
+      },
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+        decoration: BoxDecoration(
+          color:
+              isSelected ? HowWeatherColor.primary[50] : HowWeatherColor.white,
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(
+              color: isSelected
+                  ? HowWeatherColor.neutral[400]!
+                  : HowWeatherColor.neutral[200]!,
+              width: 1),
+        ),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Medium_14px(text: text),
+                isSelected
+                    ? SvgPicture.asset("assets/icons/chevron-up.svg")
+                    : SvgPicture.asset("assets/icons/chevron-down.svg"),
+              ],
+            ),
+            isSelected
+                ? Container(
+                    padding: EdgeInsets.only(top: 8),
+                    child: Column(
+                      children: [
+                        ClothInfoCard(ref, HowWeatherColor.black, "얇음"),
+                        ClothInfoCard(ref, HowWeatherColor.black, "보통"),
+                        ClothInfoCard(ref, HowWeatherColor.black, "두꺼움"),
+                      ],
+                    ))
+                : Container(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget ClothInfoCard(WidgetRef ref, color, thickness) {
+    final selected = ref.watch(selectedClothInfoProvider);
+
+    final isSelected = selected == thickness;
+
+    return InkWell(
+      onTap: () {
+        ref.read(selectedClothInfoProvider.notifier).state =
+            isSelected ? null : thickness;
+      },
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+        margin: EdgeInsets.only(bottom: 8),
+        decoration: BoxDecoration(
+          color: HowWeatherColor.white,
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(
+              color: isSelected
+                  ? HowWeatherColor.primary[900]!
+                  : HowWeatherColor.white,
+              width: 2),
+        ),
+        child: Row(
+          children: [
+            Medium_14px(text: "색깔"),
+            SizedBox(
+              width: 12,
+            ),
+            Container(
+              width: 20,
+              height: 20,
+              decoration: BoxDecoration(
+                color: color,
+                shape: BoxShape.circle,
+                border:
+                    Border.all(color: HowWeatherColor.neutral[200]!, width: 2),
+              ),
+            ),
+            Spacer(),
+            Medium_14px(text: "두께"),
+            SizedBox(
+              width: 12,
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 12, vertical: 2),
+              decoration: BoxDecoration(
+                color: HowWeatherColor.primary[600],
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Medium_14px(
+                text: thickness,
+                color: HowWeatherColor.white,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
