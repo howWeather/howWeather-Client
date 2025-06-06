354dc24f4be26dacedc6e6c63c5ccea47c5676e1
kimjihyun <kimjihyun@MacBook-Pro.local>
2025-06-06 14:15:59 +0900

    feat: 아우터 이미지 api (#106)

--- lib/api/cloth/cloth_repository.dart
+++ lib/api/cloth/cloth_repository.dart
@@ -25,4 +25,25 @@ class ClothRepository {
       throw Exception('상의 이미지 URL 반환 실패: ${response.statusCode}');
     }
   }
+
+  /// 아우터 이미지 URL 반환
+  Future<String> getOuterClothImage(int clothType) async {
+    final accessToken = await AuthStorage.getAccessToken();
+
+    final url = Uri.parse('$_baseUrl/outer-image/$clothType');
+    final response = await http.get(
+      url,
+      headers: {
+        'Authorization': 'Bearer $accessToken',
+        'Content-Type': 'application/json',
+      },
+    );
+    final decodedResponse = utf8.decode(response.bodyBytes);
+    final jsonBody = jsonDecode(decodedResponse);
+    if (response.statusCode == 200) {
+      return jsonBody['result'];
+    } else {
+      throw Exception('아우터 이미지 URL 반환 실패: ${response.statusCode}');
+    }
+  }
 }
--- lib/api/cloth/cloth_view_model.dart
+++ lib/api/cloth/cloth_view_model.dart
@@ -6,6 +6,18 @@ final clothViewModelProvider =
   (ref) => ClothNotifier(ref),
 );
 
+final upperClothImageProvider =
+    FutureProvider.family<String, int>((ref, clothType) async {
+  final repo = ClothRepository();
+  return await repo.getUpperClothImage(clothType);
+});
+
+final outerClothImageProvider =
+    FutureProvider.family<String, int>((ref, clothType) async {
+  final repo = ClothRepository();
+  return await repo.getOuterClothImage(clothType);
+});
+
 class ClothNotifier extends StateNotifier<AsyncValue<String>> {
   final Ref ref;
 
@@ -15,6 +27,18 @@ class ClothNotifier extends StateNotifier<AsyncValue<String>> {
     try {
       final repo = ClothRepository();
       final clothes = await repo.getUpperClothImage(clothType);
+      state = AsyncValue.data(clothes);
+      return clothes;
+    } catch (e, st) {
+      state = AsyncValue.error(e, st);
+      return '';
+    }
+  }
+
+  Future<String> getOuterClothImage(int clothType) async {
+    try {
+      final repo = ClothRepository();
+      final clothes = await repo.getOuterClothImage(clothType);
       state = AsyncValue.data(clothes);
       return clothes;
     } catch (e, st) {
