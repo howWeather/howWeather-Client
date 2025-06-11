de2881b425e11c0c0c0539065287e1f7f682ad12
kimjihyun <kimjihyun@MacBook-Pro.local>
2025-06-12 00:24:45 +0900

    chore: modify background color to white (#135)

--- lib/screens/skeleton/mypage_skeleton.dart
+++ lib/screens/skeleton/mypage_skeleton.dart
@@ -30,93 +30,96 @@ class MyPageSkeleton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // 닉네임 & 화살표
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              _buildShimmerBox(height: 45, width: 120),
-              _buildShimmerBox(height: 30, width: 24),
-            ],
-          ),
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 24),
-            child: Divider(
-              color: HowWeatherColor.primary[900],
-              height: 1,
+    return Container(
+      color: HowWeatherColor.white,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // 닉네임 & 화살표
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                _buildShimmerBox(height: 45, width: 120),
+                _buildShimmerBox(height: 30, width: 24),
+              ],
             ),
-          ),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 24),
+              child: Divider(
+                color: HowWeatherColor.primary[900],
+                height: 1,
+              ),
+            ),
 
-          // "나의 옷장" 헤더
-          _buildShimmerBox(height: 24, width: 100),
-          const SizedBox(height: 8),
+            // "나의 옷장" 헤더
+            _buildShimmerBox(height: 24, width: 100),
+            const SizedBox(height: 8),
 
-          // 옷장 관련 항목 3개
-          _buildShimmerBox(height: 40),
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 8),
-            child: Divider(
-              color: HowWeatherColor.neutral[200],
-              height: 1,
+            // 옷장 관련 항목 3개
+            _buildShimmerBox(height: 40),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 8),
+              child: Divider(
+                color: HowWeatherColor.neutral[200],
+                height: 1,
+              ),
             ),
-          ),
-          _buildShimmerBox(height: 40),
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 8),
-            child: Divider(
-              color: HowWeatherColor.neutral[200],
-              height: 1,
+            _buildShimmerBox(height: 40),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 8),
+              child: Divider(
+                color: HowWeatherColor.neutral[200],
+                height: 1,
+              ),
             ),
-          ),
-          _buildShimmerBox(height: 40),
+            _buildShimmerBox(height: 40),
 
-          SizedBox(
-            height: 10,
-          ),
+            SizedBox(
+              height: 10,
+            ),
 
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 24),
-            child: Divider(
-              color: HowWeatherColor.primary[900],
-              height: 1,
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 24),
+              child: Divider(
+                color: HowWeatherColor.primary[900],
+                height: 1,
+              ),
             ),
-          ),
 
-          // "설정" 헤더
-          _buildShimmerBox(height: 24, width: 60),
-          const SizedBox(height: 8),
+            // "설정" 헤더
+            _buildShimmerBox(height: 24, width: 60),
+            const SizedBox(height: 8),
 
-          // 설정 관련 항목 4개
-          _buildShimmerBox(height: 40),
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 8),
-            child: Divider(
-              color: HowWeatherColor.neutral[200],
-              height: 1,
+            // 설정 관련 항목 4개
+            _buildShimmerBox(height: 40),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 8),
+              child: Divider(
+                color: HowWeatherColor.neutral[200],
+                height: 1,
+              ),
             ),
-          ),
-          _buildShimmerBox(height: 40),
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 8),
-            child: Divider(
-              color: HowWeatherColor.neutral[200],
-              height: 1,
+            _buildShimmerBox(height: 40),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 8),
+              child: Divider(
+                color: HowWeatherColor.neutral[200],
+                height: 1,
+              ),
             ),
-          ),
-          _buildShimmerBox(height: 40),
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-            child: Divider(
-              color: HowWeatherColor.neutral[200],
-              height: 1,
+            _buildShimmerBox(height: 40),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+              child: Divider(
+                color: HowWeatherColor.neutral[200],
+                height: 1,
+              ),
             ),
-          ),
-          _buildShimmerBox(height: 40),
-        ],
+            _buildShimmerBox(height: 40),
+          ],
+        ),
       ),
     );
   }
